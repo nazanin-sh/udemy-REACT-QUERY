9984d758362e7128c5a1967fe8c590e9f63b2c8c
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-05 16:09:04 +0330

    Fetching comments for each posts

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,9 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+  if (isLoading) return <h3>Loading ...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>!!!Oops...</h3>
+        <h2>{error.toString()}</h2>
+      </>
+    );
 
   return (
     <>
@@ -12,9 +23,11 @@ export function PostDetail({ post }) {
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
+        <>
+          <li key={comment.id}>
+            {comment.email}: {comment.body}
+          </li>
+        </>
       ))}
     </>
   );
