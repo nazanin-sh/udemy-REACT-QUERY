5409404fc64cce667f0110e7edd937c6e33b3424
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-05 09:21:40 +0330

    Using tanstack

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useQuery } from "@tanstack/react-query";
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -7,10 +7,18 @@ const maxPostPage = 10;
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
-
-  // replace with useQuery
-  const data = [];
-
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+  if (isLoading) return <h3>Loading ...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>!!!Oops...</h3>
+        <h2>{error.toString()}</h2>
+      </>
+    );
   return (
     <>
       <ul>
