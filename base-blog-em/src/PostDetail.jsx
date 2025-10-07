313ff885590bf32f72aed2ddfa9811460f8ebe2b
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-07 14:56:42 +0330

    Using useMutation for deleting post

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,11 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchComments } from "./api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deletePost, fetchComments } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["comments", post.id],
     queryFn: () => fetchComments(post.id),
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
   });
   if (isLoading) return <h3>Loading ...</h3>;
   if (isError)
@@ -19,7 +23,16 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && <p style={{ color: "red" }}>Have error !</p>}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Deleted post</p>
+      )}
+      {deleteMutation.isPending && (
+        <p style={{ color: "purple" }}>Deleting post...</p>
+      )}
+
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
