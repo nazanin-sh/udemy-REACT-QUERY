e6524e089d7cdc48bb2a4f8d2cbd6836567b3d4d
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-13 12:31:03 +0330

    Centeralized error handling with queryclient(using mutationCache and queryCache in tanstack)

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import type { Treatment } from "@shared/types";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 import { useQuery } from "@tanstack/react-query";
-import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -12,22 +11,10 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
   const fallback: any = [];
-  const {
-    data = fallback,
-    isError,
-    error,
-  } = useQuery({
+  const { data = fallback } = useQuery({
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
   });
-  if (isError && error) {
-    const title =
-      error instanceof Error
-        ? error.toString().replace(/^Error:\s*/, "")
-        : "Error connecting to the server";
-    toast({ title, status: "error" });
-  }
   return data;
 }
