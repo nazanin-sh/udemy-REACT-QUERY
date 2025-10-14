0563cebf65ffe0e83161348bcbabc1318123b0a5
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-14 14:03:40 +0330

    Error handling for useQuery

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,6 +3,7 @@ import type { Treatment } from "@shared/types";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 import { useQuery } from "@tanstack/react-query";
+import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -11,10 +12,22 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
   const fallback: any = [];
-  const { data = fallback } = useQuery({
+  const {
+    data = fallback,
+    isError,
+    error,
+  } = useQuery({
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
   });
+  if (isError && error) {
+    const title =
+      error instanceof Error
+        ? error.toString().replace(/^Error:\s*/, "")
+        : "Error connecting to the server";
+    toast({ title, status: "error" });
+  }
   return data;
 }
