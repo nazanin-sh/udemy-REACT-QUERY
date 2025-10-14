b46b2035a509e067fe53c2e73da9965aae202ae5
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-14 14:06:17 +0330

    onError default for queryClient(centralized error handling)

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,7 @@ import type { Treatment } from "@shared/types";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 import { useQuery } from "@tanstack/react-query";
-import { useCustomToast } from "@/components/app/hooks/useCustomToast";
+// import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -12,7 +12,7 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
+  // const toast = useCustomToast();
   const fallback: any = [];
   const {
     data = fallback,
@@ -22,12 +22,12 @@ export function useTreatments(): Treatment[] {
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
   });
-  if (isError && error) {
-    const title =
-      error instanceof Error
-        ? error.toString().replace(/^Error:\s*/, "")
-        : "Error connecting to the server";
-    toast({ title, status: "error" });
-  }
+  // if (isError && error) {
+  //   const title =
+  //     error instanceof Error
+  //       ? error.toString().replace(/^Error:\s*/, "")
+  //       : "Error connecting to the server";
+  //   toast({ title, status: "error" });
+  // }
   return data;
 }
