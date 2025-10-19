c7ae37132f9608a0bf34febf58ef2a59ab9311a4
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-19 16:39:42 +0330

    Dependent queries

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,35 +1,48 @@
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
-
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
-
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
+  const queryClient = useQueryClient();
   // TODO: call useQuery to update user data from server
-  const user: User = null;
+  const { userId, userToken } = useLoginData();
+  console.log({ userId });
 
+  const { data: user } = useQuery({
+    queryKey: [queryKeys.user, userId],
+    queryFn: () => getUser(userId, userToken),
+    enabled: !!userId,
+    staleTime: Infinity,
+  });
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
+    queryClient.setQueryData([queryKeys.user, userId], newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    queryClient.removeQueries({ queryKey: [queryKeys.user, userId] });
+    queryClient.removeQueries({
+      queryKey: [queryKeys.appointments, queryKeys.user],
+    });
   }
 
   return { user, updateUser, clearUser };
