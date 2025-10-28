12aa6d1b5049f376521e8b6a2502368bbf32fa79
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-10-28 14:40:41 +0330

    Custom mutation hook for useReserveAppointment and its types

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,30 +1,34 @@
 import { Appointment } from "@shared/types";
-
+import { useMutation, UseMutateFunction } from "@tanstack/react-query";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance } from "@/axiosInstance";
 import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 import { queryKeys } from "@/react-query/constants";
 
-// for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? "replace" : "add";
+  const patchData = [{ op: patchOp, path: "/userId", value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
-export function useReserveAppointment() {
+export function useReserveAppointment(): UseMutateFunction<
+  void,
+  unknown,
+  Appointment,
+  unknown
+> {
   const { userId } = useLoginData();
 
   const toast = useCustomToast();
-
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    mutationFn: (appointment: Appointment) =>
+      setAppointmentUser(appointment, userId),
+  });
+  return mutate;
 }
