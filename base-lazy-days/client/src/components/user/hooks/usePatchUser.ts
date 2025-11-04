9488ad555120acf4e66627565d2e7db7bd75c65c
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-11-04 12:50:01 +0330

    Writing optimistic update for User

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -5,6 +5,7 @@ import type { User } from "@shared/types";
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
 import { useCustomToast } from "@/components/app/hooks/useCustomToast";
+import { queryKeys } from "@/react-query/constants";
 
 async function patchUserOnServer(
   newData: User | null,
@@ -33,17 +34,36 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
-
   const { mutate: patchUser } = useMutation({
     mutationFn: (newUserDate: User) => patchUserOnServer(newUserDate, user),
+    onMutate: async (newData: User | null, context) => {
+      await context.client.cancelQueries({
+        queryKey: [queryKeys.user, user.id],
+      });
+      const prevData: any = context.client.getQueryData([
+        queryKeys.user,
+        user.id,
+      ]);
+      updateUser(newData);
+      return { prevData };
+    },
+    onError: (error, _variables, context) => {
+      toast({
+        title: "update failed!!",
+        status: "warning",
+      });
+      if (context.prevData) updateUser(context?.prevData);
+    },
     onSuccess: (userData: User | null) => {
       if (user) {
-        updateUser(userData);
         toast({
           title: "User has been updated!",
           status: "success",
         });
       }
+    },
+    onSettled: (newUer, error, variables, onMutateResult, context) => {
+      context.client.invalidateQueries({ queryKey: [queryKeys.user, user.id] });
     },
   });
   return patchUser;
