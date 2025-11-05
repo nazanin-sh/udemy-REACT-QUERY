0837f9fd543c477799f274b6912199b23ffcfb50
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-11-05 10:22:49 +0330

    Test Rendered Staff Data

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,11 @@
+import { render } from "@/test-utils";
 import { AllStaff } from "../AllStaff";
+import { screen } from "@testing-library/react";
 
-// import { render, screen } from "@/test-utils";
-
-test("renders response from query", () => {
-  // write test here
+test("renders response from query", async () => {
+  render(<AllStaff />);
+  const allStaffs = await screen.findAllByRole("heading", {
+    name: /Mateo|Michael|Sandra|Divya/i,
+  });
+  expect(allStaffs).toHaveLength(4);
 });
