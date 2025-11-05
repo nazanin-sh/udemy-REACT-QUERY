f4ad7cb19ed844d15f2df058bef2be2c1c859e2d
Nazanin-sh <nazaninsadatsharifian@gmail.com>
2025-11-05 09:29:16 +0330

    Testing Rendered Query Data

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,10 @@ import { render } from "@/test-utils";
 
 import { Treatments } from "../Treatments";
 
-test("renders response from query", () => {
-  render(<Treatments />)
+test("renders response from query", async () => {
+  render(<Treatments />);
+  const treatments = await screen.findAllByRole("heading", {
+    name: /massage|facial|scrub/i,
+  });
+  expect(treatments).toHaveLength(3);
 });
